678a37f81b4b51a72fc6234545a5c135521f1af7
Richard Hyatt <richardhyatt@me.com>
2016-05-27 10:01:29 -0400

    updated tests

--- test/lib/utils.test.js
+++ test/lib/utils.test.js
@@ -45,4 +45,89 @@ describe( 'lib/utils', function() {
             expect( utils.parseJSON.bind( null, '===' + JSON.stringify( json ) ) ).to.throw();
         });
     });
+
+    describe( '.isFunction', function() {
+
+        it( 'function', function() {
+
+            expect( utils.isFunction( describe ) ).to.be.true;
+        });
+
+        it( 'other', function() {
+
+            expect( utils.isFunction( "hello" ) ).to.be.false;
+            expect( utils.isFunction( 42 ) ).to.be.false;
+            expect( utils.isFunction() ).to.be.false;
+        })
+    });
+
+    describe( '.isArray', function() {
+
+        it( 'stubbed implementation', function() {
+
+            expect( utils.isArray ).to.equal( Array.isArray );
+        });
+    });
+
+    describe( '.isPromise', function() {
+
+        it( 'promise', function() {
+
+            expect( utils.isPromise( Promise.resolve() ) ).to.be.true;
+            expect( utils.isPromise( Promise.reject() ) ).to.be.true;
+            expect( utils.isPromise( new Promise( function() {}) ) ).to.be.true;
+        });
+
+        it( 'other', function() {
+
+            expect( utils.isPromise( { then: function() {} } ) ).to.be.false;
+            expect( utils.isPromise( { catch: function() {} } ) ).to.be.false;
+            expect( utils.isPromise() ).to.be.false;
+        });
+    });
+
+    describe( '.isObject', function() {
+
+        it( 'object', function() {
+
+            expect( utils.isObject( {} ) ).to.be.true;
+            expect( utils.isObject( describe ) ).to.be.true;
+        });
+
+        it( 'other', function() {
+
+            expect( utils.isObject( 1234 ) ).to.be.false;
+            expect( utils.isObject( "fred" ) ).to.be.false;
+            expect( utils.isObject() ).to.be.false;
+        });
+    });
+
+    describe( '.isString', function() {
+
+        it( 'string', function() {
+
+            expect( utils.isString( "test" ) ).to.be.true;
+            expect( utils.isString( new Buffer( "string" ).toString() ) ).to.be.true;
+        });
+
+        it( 'other', function() {
+
+            expect( utils.isString( 1234 ) ).to.be.false;
+            expect( utils.isString( {} ) ).to.be.false;
+            expect( utils.isString( describe ) ).to.be.false;
+        });
+    });
+
+    describe( '.clone', function() {
+
+        it( 'normal operation', function() {
+
+            let v1 = { name: 'fred', age: 42 };
+
+            let v2 = utils.clone( v1 );
+
+            expect( v1 ).to.not.equal( v2 );
+            expect( v1 ).to.eql( v2 );
+        });
+    });
 });
