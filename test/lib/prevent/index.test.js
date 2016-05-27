1db12beb2408ee938f520e9ac559e32cb65426dc
Richard Hyatt <richardhyatt@me.com>
2016-05-27 10:11:06 -0400

    added state module to prevent

--- test/lib/prevent/index.test.js
+++ test/lib/prevent/index.test.js
@@ -8,6 +8,8 @@ const freshy = require( 'freshy' );
 
 const PREVENT_MODULE_PATH =  '../../../lib/prevent/index';
 
+const STATE_MODULE_PATH = '../../../lib/state';
+
 const restorer = require( './restorer' );
 
 describe( 'lib/prevent/index', function() {
@@ -26,22 +28,35 @@ describe( 'lib/prevent/index', function() {
         restorer.restore();
     });
 
+    let state;
+
+    function loadModules() {
+
+        require( PREVENT_MODULE_PATH );
+
+        state = require( STATE_MODULE_PATH );
+    }
+
     describe( 'load', function() {
 
         it( 'EVAL not set', function() {
 
-            let prevent = require( PREVENT_MODULE_PATH );
+            loadModules();
 
             expect( eval.bind( null, 'var x = 5;' ) ).to.throw( 'security violation:' );
+
+            expect( state.current.prevent ).to.eql( { eval: true } );
         });
 
         it( 'EVAL set', function() {
 
             process.env.VANDIUM_PREVENT_EVAL = false;
 
-            let prevent = require( PREVENT_MODULE_PATH );
+            loadModules();
 
             expect( eval.bind( null, 'var x = 5;' ) ).to.not.throw( 'security violation:' );
+
+            expect( state.current.prevent ).to.eql( { eval: false } );
         });
     });
 });
