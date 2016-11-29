8408c90fc2f7933ada4c6990143da734762b40c6
rhyatt <richard@vandium.io>
2016-11-29 14:23:46 -0500

    added example and test for lambda-proxy

---
+++ examples/lambda-proxy/handler.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var vandium = require( 'vandium' );
+
+exports.handler = vandium.createInstance( {
+
+            lambdaProxy: true,
+            validation: {
+
+                schema: {
+
+                    body: {
+
+                        firstName: vandium.types.string().min( 1 ).max( 250 ).required(),
+
+                        lastName: vandium.types.string().min( 1 ).max( 250 ).required(),
+
+                        age: vandium.types.number().min( 0 ).max( 130 ).required()
+                    }
+                }
+            }
+        })
+        .handler( function( event /*, context, callback*/ ) {
+
+            // log event ?
+            // console.log( JSON.stringify( event, null, 2 ) );
+
+            // echo body portion
+            return Promise.resolve( event.body );
+        });
---
+++ test/examples/lambda-proxy/handler.test.js
@@ -0,0 +1,89 @@
+'use strict';
+
+/*jshint expr: true*/
+
+const expect = require( 'chai' ).expect;
+
+const proxyquire = require( 'proxyquire' );
+
+const HandlerInvoker = require( '../../lib/handler_invoker' );
+
+describe( 'examples/lambda-proxy/handler', function() {
+
+    let handler;
+
+    beforeEach( function() {
+
+        let vandium = proxyquire( '../../../lib/index', {} );
+
+        handler = proxyquire( '../../../examples/lambda-proxy/handler', {
+
+            'vandium': vandium
+        }).handler;
+    });
+
+    describe( '.handler', function() {
+
+        it( 'normal operation', function() {
+
+            return HandlerInvoker( handler )
+                .event( {
+
+                    body: {
+
+                        firstName: 'John',
+                        lastName: 'Doe',
+                        age: 42
+                    }
+                })
+                .expectResult( (result) => {
+
+                    expect( result ).to.eql( {
+
+                        statusCode: 200,
+                        headers: {},
+                        body: '{"firstName":"John","lastName":"Doe","age":42}'
+                    });
+                });
+        });
+
+        it( 'fail when body is invalid', function() {
+
+            return HandlerInvoker( handler )
+                .event( {
+
+                    body: {
+
+                        firstName: 'John',
+                        // no lastName
+                        age: 42
+                    }
+                })
+                .expectResult( (result) => {
+
+                    expect( result ).to.eql( {
+
+                        statusCode: 422,
+                        headers: {},
+                        body: '{"type":"ValidationError","message":"validation error: child \\"lastName\\" fails because [\\"lastName\\" is required]"}'
+                    });
+                });
+        });
+
+        it( 'fail when body is missing', function() {
+
+            return HandlerInvoker( handler )
+                .event( { } )
+                .expectResult( (result) => {
+
+                    expect( result ).to.eql( {
+
+                        statusCode: 422,
+                        headers: {},
+                        body: '{"type":"ValidationError","message":"validation error: child \\"firstName\\" fails because [\\"firstName\\" is required]"}'
+                    });
+                });
+        });
+
+    });
+});
