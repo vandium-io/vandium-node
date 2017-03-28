7f54e012642146996012ba6e2a9ce0a0580be735
rhyatt <richard@vandium.io>
2017-03-28 19:07:12 -0400

    Updated

--- test/lib/event_types/api/api.test.js
+++ test/lib/event_types/api/api.test.js
@@ -6,23 +6,58 @@ var expect = require( 'chai' ).expect;
 
 const MODULE_PATH = 'lib/event_types/api/index';
 
+const vandium = require( '../../../../' );
+
 const apiHandler = require( '../../../../' + MODULE_PATH );
+
+function runHandler( handler, event, context ) {
+
+    return new Promise( (resolve, reject ) => {
+
+        handler( event, context, (err,result) => {
+
+            if( err ) {
+
+                return reject( err );
+            }
+
+            resolve( result );
+        });
+    })
+}
 
 describe( MODULE_PATH, function() {
 
     describe( 'handler', function() {
 
-        it( 'normal operation', function( done ) {
+        it( 'normal operation', function() {
 
             let handler = apiHandler();
 
-            console.log( handler );
+            handler.GET( {}, function() { return 'get called'; });
+            handler.DELETE( {}, function() { return 'delete called'; });
 
-            handler.GET( {}, function() {});
-            handler.put( {}, function() {});
-            handler.DELETE( {}, function() {});
+            handler.PUT( {
 
-            handler( {}, {}, done );
+                body: {
+
+                    name: vandium.types.string().trim().required()
+                }
+
+            }, ( evt ) => {
+
+                expect( evt.body.name ).to.equal( 'John Doe' );     // event contains padded input
+                
+                return 'put called';
+            });
+
+            let event = require( './put-event.json' );
+
+            return runHandler( handler, event, {} )
+                .then( (result) => {
+
+                    expect( result ).to.equal( 'put called' );
+                });
         });
     });
 });
