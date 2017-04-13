0f05b60324c0fca681298466a026d0b0cb1b7624
Richard Hyatt <richardhyatt@me.com>
2017-04-13 19:15:28 +0200

    added tests for lib/jwt/utils

---
+++ test/lib/jwt/utils.test.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/*jshint expr: true*/
+
+const expect = require( 'chai' ).expect;
+
+const utils = require( '../../../lib/jwt/utils' );
+
+describe( 'lib/event_types/index', function() {
+
+    describe( '.resolveAlgorithm', function() {
+
+        [ 'RS256', 'HS256', 'HS384', 'HS512' ].forEach( ( algorithm ) => {
+
+            it( `algorithm is ${algorithm}`, function() {
+
+                let alg = utils.resolveAlgorithm( algorithm );
+
+                expect( alg ).to.equal( algorithm );
+            });
+        });
+
+        it( 'fail when algorithm is unknown', function() {
+
+            expect( utils.resolveAlgorithm.bind( null, 'whatever' ) ).to.throw( 'authentication error: jwt algorithm "whatever" is unsupported' );
+        });
+
+        it( 'fail when algorithm is missing', function() {
+
+            expect( utils.resolveAlgorithm.bind( null ) ).to.throw( 'authentication error: missing algorithm' );
+        });
+    });
+});
