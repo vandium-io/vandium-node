a5d42cf5bc5b7dbe2db20988adea563185ce3ec7
Richard Hyatt <richardhyatt@me.com>
2017-04-19 14:38:58 +0200

    updated and added tests

---
+++ test/lib/event_types/typed.test.js
@@ -0,0 +1,79 @@
+'use strict';
+
+/*jshint expr: true*/
+
+const expect = require( 'chai' ).expect;
+
+const proxyquire = require( 'proxyquire' );
+
+const sinon = require( 'sinon' );
+
+
+describe( 'lib/event_types/typed', function() {
+
+    let TypedHandler;
+
+    let eventIdentifierStub;
+
+    beforeEach( function() {
+
+        eventIdentifierStub = {
+
+            identify: sinon.stub()
+        };
+
+        TypedHandler = proxyquire( '../../../lib/event_types/typed', {
+
+            '@vandium/event-identifier': eventIdentifierStub
+        })
+    });
+
+    describe( 'constructor', function() {
+
+        it( 'normal operation', function() {
+
+            let instance = new TypedHandler( 'my-type', {} );
+
+            expect( instance._type ).to.equal( 'my-type' );
+        });
+    });
+
+    describe( '.executePreprocessors', function() {
+
+        it( 'matching event type', function() {
+
+            let instance = new TypedHandler( 's3' );
+
+            let state = {
+
+                event: { whatever: true },
+                context: {}
+            };
+
+            eventIdentifierStub.identify.returns( 's3' );
+
+            instance.executePreprocessors( state );
+
+            expect( eventIdentifierStub.identify.calledOnce ).to.be.true;
+            expect( eventIdentifierStub.identify.firstCall.args ).to.eql( [ state.event ] );
+        });
+
+        it( 'non-matching event type', function() {
+
+            let instance = new TypedHandler( 's3' );
+
+            let state = {
+
+                event: { whatever: true },
+                context: {}
+            };
+
+            eventIdentifierStub.identify.returns( 'sns' );
+
+            expect( instance.executePreprocessors.bind( instance, state ) ).to.throw( 'Expected event type of s3 but identified as sns' );
+
+            expect( eventIdentifierStub.identify.calledOnce ).to.be.true;
+            expect( eventIdentifierStub.identify.firstCall.args ).to.eql( [ state.event ] );
+        });
+    });
+});
