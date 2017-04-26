0d19b08918165c1b213c27d18fa22a1425b55f68
rhyatt <richard@vandium.io>
2017-04-26 13:22:51 -0400

    added way to disable JWT even if enabled via environment vars

--- test/lib/event_types/api/jwt.test.js
+++ test/lib/event_types/api/jwt.test.js
@@ -48,6 +48,14 @@ describe( MODULE_PATH, function() {
                 expect( instance.enabled ).to.be.false;
             });
 
+            it( 'enabled = false', function() {
+
+                let instance = new JWTValidator( { enabled: false } );
+
+                expect( instance ).to.be.instanceof( JWTValidator );
+                expect( instance.enabled ).to.be.false;
+            });
+
             it( 'no options, env vars set for algorithm and secret', function() {
 
                 process.env.VANDIUM_JWT_ALGORITHM = 'HS256';
