2eeeed2dfed7548bc8e1450b1312904c981d9a73
Richard Hyatt <richardhyatt@me.com>
2019-12-01 19:46:43 -0500

    updated

--- examples/api-gateway/handler.js
+++ examples/api-gateway/handler.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const vandium = require( 'vandium' );
 
 function getUsers( offset, expand ) {
@@ -17,51 +15,22 @@ function getUsers( offset, expand ) {
     return Promise.resolve( users );
 }
 
-function addUser( firstName, lastName, age ) {
-
-    // TODO: add user to database
-
-    return Promise.resolve( { id: Date.now(), firstName, lastName, age } );
-}
-
 exports.handler = vandium.api()
-        .GET( {
+        .GET()
+        .validation({
 
-                queryStringParameters: {
+            queryStringParameters: {
 
-                    offset: vandium.types.number(),
-                    expand: vandium.types.boolean()
-                }
-            },
-            (event) => {
+                offset: vandium.types.number(),
+                expand: vandium.types.boolean()
+            }
+        })
+        .handler( async (event) => {
 
-                let offset = event.queryStringParameters.offset;
-                let expand = event.queryStringParameters.expand;
+            let offset = event.queryStringParameters.offset;
+            let expand = event.queryStringParameters.expand;
 
-                return getUsers( offset, expand )
-                    .then( (users) => {
+            const users = await getUsers( offset, expand );
 
-                        return {
-
-                            users,
-                            offset
-                        };
-                    });
-            })
-        .POST( {
-
-                body: {
-
-                    firstName: vandium.types.string().min(1).max(250).required(),
-                    lastName: vandium.types.string().min(1).max(250).required(),
-                    age: vandium.types.number().min(0).max(130)
-                }
-            },
-            (event) => {
-
-                let firstName = event.body.firstName;
-                let lastName = event.body.lastName;
-                let age = event.body.age;
-
-                return addUser( firstName, lastName, age );
-            });
+            return { users, offset };
+        });
